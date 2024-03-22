4b26821a9c16d3f8be1034b69f6e8d1a9507cdcc
mariahisenberg <isenberg_mariah@student.mahoningctc.com>
2024-03-22 13:49:50 -0400

    initial commit

---
+++ mycode.py
@@ -0,0 +1,19 @@
+my_int= 25
+my_float=3.14
+
+print(my_int)
+print(my_float)
+print(my_int+my_float)
+print(my_int-my_float)
+
+first_name="Mariah"
+last_name="Isenberg"
+full_name=first_name+" "+last_name
+print(full_name)
+
+print(len(full_name))
+
+starships=(["millenium falcon", "star destroyer", "x-wing"])
+starships.append("TIE Fighter")
+print(starships)
+print(starships[1])
